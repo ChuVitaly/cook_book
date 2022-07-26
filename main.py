ae92d63bee624702990e5a1f2967ac7ed25a96e5
Виталий Чухляев <vitaliy69@mail.ru>
2022-07-26 09:14:35 +0300

    I did the first task. I can't find the error

---
+++ main.py
@@ -0,0 +1,24 @@
+cook_book = {}
+cook_book_keys = []
+list_values = []
+list_keys = ['ingredient_name', 'quantity', 'measure']
+
+with open("recipes.txt", "r") as file:
+    for line in file:
+        if line == "\n" or line == " ":
+            continue
+        key = line.rstrip()
+        cook_book_keys.append(key)
+        count = int(file.readline())
+        i = 0
+        while i < count:
+            b = ((file.readline()).rstrip()).replace("|", " ")
+            b = b.split()
+            new_dict = dict(zip(list_keys, b))
+            list_values.append(new_dict)
+            i += 1
+        cook_book[f"{key}"] = list_values
+
+
+
+print(cook_book )
