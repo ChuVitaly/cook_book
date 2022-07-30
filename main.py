748544ea28d537235b39deabab0112686c85b043
Виталий Чухляев <vitaliy69@mail.ru>
2022-07-30 12:43:12 +0300

    Получилось вывести все ключи словаря "cook_book"

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 cook_book = {}
 cook_book_keys = []
-list_values = []
+# list_values = []
 list_keys = ['ingredient_name', 'quantity', 'measure']
 
 with open("recipes.txt", "r") as file:
     for line in file:
         if line == "\n" or line == " ":
             continue
+        cook_book_keys.clear()
         key = line.rstrip()
         cook_book_keys.append(key)
         count = int(file.readline())
         i = 0
+        list_values = []
+        list_values.clear()
         while i < count:
             b = ((file.readline()).rstrip()).replace("|", " ")
             b = b.split()
@@ -18,7 +21,4 @@ with open("recipes.txt", "r") as file:
             list_values.append(new_dict)
             i += 1
         cook_book[f"{key}"] = list_values
-
-
-
-print(cook_book )
+print(cook_book)
